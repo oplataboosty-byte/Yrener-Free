558626cd926fa10028a10e1406eda772f8cfd7f0
oplataboosty-byte <oplataboosty@gmail.com>
2026-05-13 22:35:53 +0500

    Initial commit - Yrener project

---
+++ app/src/main/cpp/build/CMakeFiles/Yrener.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/data/data/com.termux/files/home/ree-open-main/app/src/main/cpp/func/bools.cpp" "CMakeFiles/Yrener.dir/func/bools.cpp.o" "gcc" "CMakeFiles/Yrener.dir/func/bools.cpp.o.d"
+  "/data/data/com.termux/files/home/ree-open-main/app/src/main/cpp/main.cpp" "CMakeFiles/Yrener.dir/main.cpp.o" "gcc" "CMakeFiles/Yrener.dir/main.cpp.o.d"
+  "/data/data/com.termux/files/home/ree-open-main/app/src/main/cpp/wLibs/ImGui/backends/imgui_impl_opengl3.cpp" "CMakeFiles/Yrener.dir/wLibs/ImGui/backends/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/Yrener.dir/wLibs/ImGui/backends/imgui_impl_opengl3.cpp.o.d"
+  "/data/data/com.termux/files/home/ree-open-main/app/src/main/cpp/wLibs/ImGui/imgui.cpp" "CMakeFiles/Yrener.dir/wLibs/ImGui/imgui.cpp.o" "gcc" "CMakeFiles/Yrener.dir/wLibs/ImGui/imgui.cpp.o.d"
+  "/data/data/com.termux/files/home/ree-open-main/app/src/main/cpp/wLibs/ImGui/imgui_draw.cpp" "CMakeFiles/Yrener.dir/wLibs/ImGui/imgui_draw.cpp.o" "gcc" "CMakeFiles/Yrener.dir/wLibs/ImGui/imgui_draw.cpp.o.d"
+  "/data/data/com.termux/files/home/ree-open-main/app/src/main/cpp/wLibs/ImGui/imgui_tables.cpp" "CMakeFiles/Yrener.dir/wLibs/ImGui/imgui_tables.cpp.o" "gcc" "CMakeFiles/Yrener.dir/wLibs/ImGui/imgui_tables.cpp.o.d"
+  "/data/data/com.termux/files/home/ree-open-main/app/src/main/cpp/wLibs/ImGui/imgui_widgets.cpp" "CMakeFiles/Yrener.dir/wLibs/ImGui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/Yrener.dir/wLibs/ImGui/imgui_widgets.cpp.o.d"
+  "/data/data/com.termux/files/home/ree-open-main/app/src/main/cpp/wLibs/iconvlite.cpp" "CMakeFiles/Yrener.dir/wLibs/iconvlite.cpp.o" "gcc" "CMakeFiles/Yrener.dir/wLibs/iconvlite.cpp.o.d"
+  "" "libYrener.so" "gcc" "CMakeFiles/Yrener.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ app/src/main/cpp/build/CMakeFiles/Yrener.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Yrener.dir/link.d"
+  "CMakeFiles/Yrener.dir/func/bools.cpp.o"
+  "CMakeFiles/Yrener.dir/func/bools.cpp.o.d"
+  "CMakeFiles/Yrener.dir/main.cpp.o"
+  "CMakeFiles/Yrener.dir/main.cpp.o.d"
+  "CMakeFiles/Yrener.dir/wLibs/ImGui/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/Yrener.dir/wLibs/ImGui/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/Yrener.dir/wLibs/ImGui/imgui.cpp.o"
+  "CMakeFiles/Yrener.dir/wLibs/ImGui/imgui.cpp.o.d"
+  "CMakeFiles/Yrener.dir/wLibs/ImGui/imgui_draw.cpp.o"
+  "CMakeFiles/Yrener.dir/wLibs/ImGui/imgui_draw.cpp.o.d"
+  "CMakeFiles/Yrener.dir/wLibs/ImGui/imgui_tables.cpp.o"
+  "CMakeFiles/Yrener.dir/wLibs/ImGui/imgui_tables.cpp.o.d"
+  "CMakeFiles/Yrener.dir/wLibs/ImGui/imgui_widgets.cpp.o"
+  "CMakeFiles/Yrener.dir/wLibs/ImGui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/Yrener.dir/wLibs/iconvlite.cpp.o"
+  "CMakeFiles/Yrener.dir/wLibs/iconvlite.cpp.o.d"
+  "libYrener.pdb"
+  "libYrener.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Yrener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
